3a8cd468ec3ac563f84dd0fe35a2c6be216e0175
helptrain <87045444+helptrain@users.noreply.github.com>
2022-03-21 19:33:19 -0400

    Added Solution

---
+++ RPS_Project/RPS_Console_App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPS_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
