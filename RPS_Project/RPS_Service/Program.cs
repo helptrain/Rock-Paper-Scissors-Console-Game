81e18a792287d4e761cb7b7e9db1de42b6017a6d
helptrain <87045444+helptrain@users.noreply.github.com>
2022-03-21 20:25:28 -0400

    Changed To Framework
    deleted all the projects and added new .net framework versions

--- RPS_Project/RPS_Service/Program.cs
+++ RPS_Project/RPS_Service/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RPS_Service
 {
@@ -6,7 +10,6 @@ namespace RPS_Service
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
