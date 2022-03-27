64bb9198ca8cccc3b76b2384072280abc57b09bf
helptrain <87045444+helptrain@users.noreply.github.com>
2022-03-26 23:11:13 -0400

    Added Starter service code
    added server host code

--- RPS_Project/RPS_Service/Program.cs
+++ RPS_Project/RPS_Service/Program.cs
@@ -12,6 +12,27 @@ namespace RPS_Service
     {
         static void Main(string[] args)
         {
+            ServiceHost servHost = null;
+
+            try
+            {
+                servHost = new ServiceHost(typeof(Game));
+
+                // Run the service
+                servHost.Open();
+                Console.WriteLine("Service started. Please any key to quit.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Wait for a keystroke
+                Console.ReadKey();
+                if (servHost != null)
+                    servHost.Close();
+            }
 
         }
     }
