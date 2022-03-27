c6c4ac098fb32153459d783015fb473ac22fdc41
helptrain <87045444+helptrain@users.noreply.github.com>
2022-03-26 23:07:45 -0400

    Added Player and Game Class
    player and game class created

---
+++ RPS_Project/RPS_Library/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS_Library
+{
+    public class Game
+    {
+        public Player playerOne;
+        public Player playerTwo;
+
+        public Player winner;
+
+        public Game(Player p1, Player p2)
+        {
+            playerOne = p1;
+            playerTwo = p2;
+        }
+
+        public string Playing()
+        {
+            if(playerOne.HandSignal == HandSignalType.Rock && playerTwo.HandSignal == HandSignalType.Scissors)
+            {
+                this.winner = playerOne;
+            }
+            else if(playerOne.HandSignal == HandSignalType.Paper && playerTwo.HandSignal == HandSignalType.Rock)
+            {
+                this.winner = playerOne;
+            }
+            else if (playerOne.HandSignal == HandSignalType.Scissors && playerTwo.HandSignal == HandSignalType.Paper)
+            {
+                this.winner = playerOne;
+            }
+            else if (playerOne.HandSignal == HandSignalType.Rock && playerTwo.HandSignal == HandSignalType.Paper)
+            {
+                this.winner = playerTwo;
+            }
+            else if (playerOne.HandSignal == HandSignalType.Paper && playerTwo.HandSignal == HandSignalType.Scissors)
+            {
+                this.winner = playerTwo;
+            }
+            else if (playerOne.HandSignal == HandSignalType.Scissors && playerTwo.HandSignal == HandSignalType.Rock)
+            {
+                this.winner = playerTwo;
+            }
+
+            return $"The winner is: {winner.PlayerName}";
+
+        }
+
+
+    }
+}
---
+++ RPS_Project/RPS_Library/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS_Library
+{
+    public class Player
+    {
+        public string PlayerName;
+        public HandSignalType HandSignal;
+
+        public Player(string name)
+        {
+            PlayerName = name;
+        }
+
+        public void PlaySignal(HandSignalType playedtype)
+        {
+            HandSignal = playedtype;
+        }
+    }
+}
