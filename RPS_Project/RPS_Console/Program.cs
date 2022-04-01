d685974d915ed960ca1f6a05f25e5a623cd971f8
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2022-03-31 21:11:59 -0400

    added a few functions to client

--- RPS_Project/RPS_Console/Program.cs
+++ RPS_Project/RPS_Console/Program.cs
@@ -11,24 +11,76 @@ using RPS_Library;
 
 namespace RPS_Console
 {
-    public class Program
+    public partial class Program : ICallback
     {
         // Member variables
         private static IGame game = null;
+        private string player = "John";
+        //private string player2 = "Adam";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Test");
 
             try
-            {
+            {              
                 Console.ReadLine();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex);
             }
+        }
 
+        // TODO: complete this 
+        public void SendChoice(HandSignalType[] messages)
+        {
+            try
+            {
+                Console.WriteLine("sendChoice: " + messages);
+            } catch(Exception e)
+            {
+                Console.WriteLine("error in sendChoice: " + e);
+            }
+        }
+        
+
+        private void UserExited()
+        {
+            if (game != null)
+            {
+                // TODO: not sure what is supposed to be passed in 
+                game.Leave(player);
+                //game.Leave(player2);
+                Console.WriteLine("exited");
+            }             
+        }
+
+        // Sets up a connection to the WCF service and subscribes to the callback messages
+        private void connectToGame()
+        {
+            try
+            {
+                // Configure the ABCs of using the MessageBoard service
+                DuplexChannelFactory<IGame> channel = new DuplexChannelFactory<IGame>(this, "GameService");
+
+                game = channel.CreateChannel();
+
+                if (game.Join(player))
+                {
+                    Console.WriteLine("get all msgs: " + game.GetAllChoices());
+                }
+                else
+                {
+                    // Alias rejected by the service so nullify service proxies
+                    game = null;
+                    Console.WriteLine("ERROR: Alias in use. Please try again.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error: " + e.Message);
+            }
         }
     }
 }
